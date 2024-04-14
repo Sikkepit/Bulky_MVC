ff85791f548a967eba6b90ae2703c65fe244075b
Dennis <148398668+Sikkepit@users.noreply.github.com>
2024-04-14 21:55:07 +0200

    Day One baby

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {   
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.DisplayOrder==null) {
+                ModelState.AddModelError("DisplayOrder", "This field cannot be empty");
+            }
+
+            if(ModelState.IsValid) {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }   
+            return View();
+            
+            
+        }
+    }
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Category name is a required field.")]
+        [MaxLength(30, ErrorMessage = "The category name can not be longer than 30 characters.")]
+        [DisplayName("Category Name")]
+        public required string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display order must be between 1 and 100")]
+        [Required]
+        public int? DisplayOrder { get; set; }   
+    }
+}
